c0adf7ed1bfe9cf8da075f14d51fef2371f36206
Yuto <wayne71112@gmail.com>
2026-06-08 20:53:43 +0800

    test: remove UI rendering coverage

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -38,10 +38,6 @@ class RecordingRenderer:
         self.calls.append(("thinking_stop",))
 
 
-def test_terminal_sink_implements_protocol():
-    assert isinstance(TerminalSink(), OutputSink)
-
-
 def test_silent_sink_implements_protocol():
     assert isinstance(SilentSink(), OutputSink)
 
@@ -54,36 +50,6 @@ def test_silent_sink_produces_no_output(capsys):
     sink.on_tool_result("id1", ShellResult("result"))
     sink.on_status("status msg")
     assert capsys.readouterr().out == ""
-
-
-def test_terminal_sink_on_assistant(capsys):
-    TerminalSink().on_assistant("hello")
-    assert "hello" in capsys.readouterr().out
-
-
-def test_terminal_sink_on_assistant_empty_no_output(capsys):
-    TerminalSink().on_assistant("")
-    assert capsys.readouterr().out == ""
-
-
-def test_terminal_sink_on_think(capsys):
-    TerminalSink().on_think("reasoning")
-    assert "reasoning" in capsys.readouterr().out
-
-
-def test_terminal_sink_on_tool_call(capsys):
-    TerminalSink().on_tool_call("id1", "bash", {"cmd": "ls"})
-    assert "bash" in capsys.readouterr().out
-
-
-def test_terminal_sink_on_tool_result(capsys):
-    TerminalSink().on_tool_result("id1", ShellResult("some output"))
-    assert "some output" in capsys.readouterr().out
-
-
-def test_terminal_sink_on_status(capsys):
-    TerminalSink().on_status("compacting...")
-    assert "compacting..." in capsys.readouterr().out
 
 
 def test_terminal_sink_delegates_to_renderer():
